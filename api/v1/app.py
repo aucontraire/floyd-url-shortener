848a88be07c329097df582e908e9604193298447
root <root@ip-172-31-38-147.us-east-2.compute.internal>
2018-08-31 21:12:42 +0000

    pushing updates to api - CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,5 +20,12 @@ cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 def status():
     return jsonify({"status": "ok"})
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
 if __name__ == "__main__":
     app.run(port=5003)
